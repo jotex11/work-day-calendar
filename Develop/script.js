b18fd838c272c49ba85b3c8ec19c64dd7600a27d
joseph <jomill0723@outlook.com>
2023-07-31 22:09:04 -0400

    Finished HTML, finished JS, added screenshots to readme and added URLS

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,52 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+// var definitions 
+var currentDayEl = dayjs();
+var saveBtn = $(".saveBtn");
+$("#currentDay").text(currentDayEl.format('MMM D, YYYY'));
+
+// Save button function  
+  saveBtn.on("click", function() {
+  var text = $(this).siblings(".description").val();
+  var time = $(this).siblings(".hour").text();
+  localStorage.setItem(time, text);
+})
+
+// Time tracking function
+function timeTracker(){
+var currentTimeEl = dayjs().format('hh:mm:ss');
+$(".time-block").each(function(){
+  var blockTime = parseInt($(this).attr("id"));
+  if (blockTime < currentTimeEl) {
+    $(this).addClass("future");
+  }else if (blockTime === currentTimeEl){
+    $(this).addClass("present");
+  }else {
+    $(this).addClass("past");
+  }
+})
+};
+
+// Function to save text to storage after refresh
+function saveTime() {
+  $(".hour").each(function() {
+    var currentTime2 = $(this).text();
+    var textEl = localStorage.getItem(currentTime2);
+    if (textEl !== null) {
+      $(this).siblings(".description").val(textEl);
+    }
+  })
+}
+// Get items from local storage
+$("hour-9 .description").val(localStorage.getItem("hour-9"));
+$("hour-10 .description").val(localStorage.getItem("hour-10"));
+$("hour-11 .description").val(localStorage.getItem("hour-11"));
+$("hour-12 .description").val(localStorage.getItem("hour-12"));
+$("hour-1 .description").val(localStorage.getItem("hour-1"));
+$("hour-2 .description").val(localStorage.getItem("hour-2"));
+$("hour-3 .description").val(localStorage.getItem("hour-3"));
+$("hour-4 .description").val(localStorage.getItem("hour-4"));
+$("hour-5 .description").val(localStorage.getItem("hour-5"));
+
+// Calling functions 
+timeTracker();
+saveTime();
+
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # work-day-calender
+
+## Task
+My task is to create a work day scheduele that tracks the time and date and keeps whatever data inputed in the textbox.
+This scheduele will only be from 9AM to 5PM.
+
+## URLS 
+Link to github: https://github.com/jotex11/work-day-calendar
+Link to deployed website: 
+
+## Screenshots
+![Alt text](<Screenshot 2023-07-31 at 10.07.15 PM.png>)
